96a3d5c67b432d3a94d87a0d1c0f6d237f8cff70
Seowon123 <ekdnjs7647@gmail.com>
2023-07-12 17:19:48 +0900

    init : default setting

---
+++ src/main/java/com/boardproject/_core/util/DBInit.java
@@ -0,0 +1,30 @@
+package com.boardproject._core.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import com.boardproject.user.User;
+import com.boardproject.user.UserRepository;
+
+@RequiredArgsConstructor
+@Component
+public class DBInit {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    CommandLineRunner initDB(UserRepository userRepository){
+        return args -> {
+            User ssar = User.builder()
+                    .username("ssar")
+                    .password(passwordEncoder.encode("1234"))
+                    .email("ssar@nate.com")
+                    .nickName("보리")
+                    .roles("새싹회원")
+                    .build();
+            userRepository.save(ssar);
+        };
+    }
+}
---
+++ src/main/java/com/boardproject/user/User.java
@@ -0,0 +1,60 @@
+package com.boardproject.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@Getter
+@Table(name = "user_tb")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 120)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+    @Column(nullable = false, length = 20)
+    private String nickName;
+
+    private String roles;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public User(Long id, String username, String password, String email, String nickName, String roles, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.nickName = nickName;
+        this.roles = roles;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
