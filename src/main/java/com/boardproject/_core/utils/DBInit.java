4db9d92aafbc41426fe77c1b94703cab3676270f
chaewon <djacltk@naver.com>
2023-07-12 20:52:35 +0900

    feat: DBInit에 게시글 작성 관련 코드 추가

--- src/main/java/com/boardproject/_core/utils/DBInit.java
+++ src/main/java/com/boardproject/_core/utils/DBInit.java
@@ -1,5 +1,7 @@
 package com.boardproject._core.utils;
 
+import com.boardproject.board.Board;
+import com.boardproject.board.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +10,21 @@ import org.springframework.stereotype.Component;
 import com.boardproject.user.User;
 import com.boardproject.user.UserRepository;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.boardproject._core.utils.FileHandler.FILES_PATH;
+import static com.boardproject._core.utils.FileHandler.SEPARATOR_CHAR;
+
 @RequiredArgsConstructor
 @Component
 public class DBInit {
-
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     @Bean
-    CommandLineRunner initDB(UserRepository userRepository){
+    CommandLineRunner initDB(){
         return args -> {
             User ssar = User.builder()
                     .username("ssar")
@@ -25,6 +34,24 @@ public class DBInit {
                     .roles("새싹회원")
                     .build();
             userRepository.save(ssar);
+
+            Path path = Paths.get(FILES_PATH+SEPARATOR_CHAR +"f5137072-fc18-4f96-b4be-135b03899ae9_BE 5기 QR.png");
+            Board boardWithThumbnail = Board.builder()
+                    .userId(1L)
+                    .catagory("새싹회원")
+                    .title("썸네일 있는 글")
+                    .content("<p>하이하이</p>")
+                    .thumbnail(path.toRealPath().toString())
+                    .build();
+            boardRepository.save(boardWithThumbnail);
+
+            Board boardNoThumbnail = Board.builder()
+                    .userId(1L)
+                    .catagory("새싹회원")
+                    .title("썸네일 없는 글")
+                    .content("<p>썸네일이 없어요!</p>")
+                    .build();
+            boardRepository.save(boardNoThumbnail);
         };
     }
 }
--- src/main/java/com/boardproject/_core/utils/FileHandler.java
+++ src/main/java/com/boardproject/_core/utils/FileHandler.java
@@ -8,13 +8,12 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class FileHandler {
-    private static final String projectRootPath = System.getProperty("user.dir");
-    private static final char separator = File.separatorChar;   // 운영체제에 맞추기 위함
-    private static String filesPath = "src/main/resources/static/files";
+    private static final String LOCAL_FILES_PATH = "src/main/resources/static/files";
+    public static final String PROJECT_ROOT_PATH = System.getProperty("user.dir");
+    public static final char SEPARATOR_CHAR = File.separatorChar;   // 운영체제에 맞추기 위함
+    public static final String FILES_PATH = PROJECT_ROOT_PATH + SEPARATOR_CHAR + LOCAL_FILES_PATH.replace('/', SEPARATOR_CHAR);
 
     public static String save(MultipartFile file)  {
-        String resourcePath = projectRootPath+separator+filesPath.replace('/',separator);
-
         // 랜덤 식별자
         UUID uuid = UUID.randomUUID();
 
@@ -22,7 +21,7 @@ public class FileHandler {
         String fileName = uuid + "_" + file.getOriginalFilename();
 
         // 새 파일명과 경로를 지정하여 파일 생성
-        File saveFile = new File(resourcePath, fileName);
+        File saveFile = new File(FILES_PATH, fileName);
 
         try {
             file.transferTo(saveFile);
