3f7f18aaa366efc2cfc1f40e33085cc1fa20ea38
chaewon <djacltk@naver.com>
2023-07-14 22:24:20 +0900

    refactor: 썸머노트 사진 업로드 적용

--- src/main/java/com/boardproject/File/FileController.java
+++ src/main/java/com/boardproject/File/FileController.java
@@ -1,24 +1,29 @@
 package com.boardproject.File;
 
-import com.boardproject._core.utils.ApiUtils;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/file")
 @RequiredArgsConstructor
 @RestController
 public class FileController {
     private final FileService fileService;
+
+    @PostMapping(value="/uploadSummernoteImageFile", produces = "application/json")
+    public JsonObject uploadSummernoteImageFile(@RequestParam MultipartFile file){
+        JsonObject jsonObject = fileService.save(file);
+        return jsonObject;
+    }
+
     @GetMapping("/load")
-    public ResponseEntity<?> load(@RequestParam String url){
-        FileResponse.loadDTO loadDTO = fileService.load(url);
+    public ResponseEntity<?> load(@RequestParam String path, @RequestParam String name){
+        FileResponse.loadDTO loadDTO = fileService.load(path,name);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_TYPE, loadDTO.getContentType());
