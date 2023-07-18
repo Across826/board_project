a5f80991e1bdc243d3d31bd4732756cad7e90c82
Eom Chae Won <djacltk@naver.com>
2023-07-18 23:58:25 +0900

    feat: 게시판 기능 (#5)
    * feat: db 연결 설정
    
    * feat: _core 패키지 추가(공통 응답,예외처리)
    
    * 게시글 작성 기능 (#1)
    
    * feat: 게시글 쓰기 페이지 썸머노트(summernote) 적용
    
    * feat: db 연결 설정
    
    * feat: Board 모델링
    
    * feat: Board 컬럼 수정
    
    * feat: 게시글 작성 view
    
    * feat: _core 패키지 추가(공통 응답,예외처리)
    
    * feat: Controller 구현
    
    * refactor: 등급 타입 패키지 변경
    
    * feat: Service 구현 완료
    
    * fix: Conflict 제거를 위한 수정
    
    * init : default setting (#2)
    
    * feat: H2 dependency 추가
    
    * move: Utils 패키지 정리
    
    * refactor: 게시글 작성  (#3)
    
    * feat: DBInit에 게시글 작성 관련 코드 추가
    
    * feat: 시큐리티 유저 바인딩 추가
    
    * feat: 게시글 수정&삭제, 상세보기, 댓글 (#4)
    
    * feat: 게시글 상세보기 화면 구현
    
    * feat: 상세보기 화면 연결(게시글 내용 바인딩)
    
    * feat: 게시글 삭제 구현
    
    * feat: 게시글 수정 구현
    
    * refactor: 썸머노트 사진 업로드 적용
    
    * feat: 게시글 삭제 시 관련된 모든 사진 삭제
    
    * feat: 게시글 수정 시 썸네일 선택(수정) 가능
    
    * feat: 댓글 등록&리스트 보기
    
    * feat: 댓글 삭제 구현
    
    * feat: 대댓글 구현
    
    ---------
    
    Co-authored-by: Seowon123 <85150482+Seowon123@users.noreply.github.com>

---
+++ src/main/java/com/boardproject/board/Board.java
@@ -0,0 +1,70 @@
+package com.boardproject.board;
+
+import com.boardproject.comment.Comment;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @Column(name = "user_id",nullable = false)
+    private Long userId;
+
+    private String thumbnail;
+
+    @Column(nullable = false )
+    @ColumnDefault("'GENERAL'")
+    private String catagory; // 새싹:GENERAL, 우수:VIP
+
+    @Column(name="hide_flag",nullable = false)
+    @ColumnDefault("0")
+    private int isHide; // 0:보임, 1:숨김/삭제
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @OrderBy("createdAt asc")
+    private List<Comment> commentList;
+
+    @Column(name="created_at",nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name="updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Builder
+    public Board(Long id, String title, String content, Long userId, String thumbnail,
+                 String catagory, int isHide, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.thumbnail = thumbnail;
+        this.catagory = catagory;
+        this.isHide = isHide;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/com/boardproject/board/BoardController.java
@@ -0,0 +1,68 @@
+package com.boardproject.board;
+
+import com.boardproject._core.security.CustomUserDetails;
+import com.boardproject._core.utils.JsoupParser;
+import com.boardproject.board.dto.BoardRequest;
+import com.boardproject.board.dto.BoardResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/create")
+    public String create(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        model.addAttribute("user", userDetails.getUser());
+        return "board/boardCreateForm";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid BoardRequest.CreateFormDTO createFormDTO, Errors errors) {
+        BoardResponse.CreateDTO boradDTO = boardService.create(createFormDTO);
+        return "redirect:/board/" + boradDTO.getId();
+    }
+
+    @GetMapping("/{id}")
+    public String read(Model model, @PathVariable("id") Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        BoardResponse.DetailsDTO boardDTO = boardService.getDetailsById(id);
+
+        model.addAttribute("board", boardDTO);
+        model.addAttribute("user", userDetails.getUser());
+        return "board/boardDetails";
+    }
+
+    @PostMapping("/update/form")
+    public String updateForm(Model model, @Valid BoardRequest.UpdateFormDTO updateDTO, Errors errors) {
+        model.addAttribute("board", updateDTO);
+
+        List<String> parsedImgName = JsoupParser.parseImgName(updateDTO.getContent());
+        model.addAttribute("imgList", parsedImgName);
+        return "board/boardUpdateForm";
+    }
+
+    @PostMapping("/update")
+    public String update(@Valid BoardRequest.UpdateDTO updateDTO, Errors errors) {
+        BoardResponse.UpdateDTO update = boardService.update(updateDTO);
+        return "redirect:/board/" + update.getId();
+    }
+
+    // @Todo 삭제 후 목록 보기로 이동 (목록 보기 url 연결 필요)
+    @PostMapping("/delete")
+    public String delete(@Valid BoardRequest.DeleteDTO deleteDTO, Errors errors) {
+        boardService.delete(deleteDTO);
+        return "redirect:";
+    }
+}
